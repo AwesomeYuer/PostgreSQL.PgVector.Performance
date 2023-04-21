5c7da4271c307c26377d8d654f225f02b1955e2b
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-04-22 07:46:54 +0800

    1

--- PostgreSQL.PgVector.Performance/GlobalManager.cs
+++ PostgreSQL.PgVector.Performance/GlobalManager.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace PostgreSQL.PgVector.Performance;
 
-namespace PostgreSQL.PgVector.Performance
+public static class GlobalManager
 {
-    public static class GlobalManager
-    {
-        public static string ConnectionString = "Host=localhost;Database=pgvectors;User Id=sa;Password=2022db@Qwer";
-    }
+    public static string ConnectionString = "Host=localhost;Database=pgvectors;User Id=sa;Password=2022db@Qwer";
 }
