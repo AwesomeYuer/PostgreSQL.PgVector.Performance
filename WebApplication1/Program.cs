8fac59ec83eeed454edd7b0fa7c5a1df75fbc542
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-04-22 15:56:13 +0800

    1

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,34 @@
+using PostgreSQL.PgVector.Performance;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app
+    .MapGet
+        (
+              "/pgsql"
+            , async () =>
+            {
+                await new TestContext().ProcessAsync();
+                return "ok";
+            }
+        )
+.WithName("pgsql");
+
+app.Run();
+
