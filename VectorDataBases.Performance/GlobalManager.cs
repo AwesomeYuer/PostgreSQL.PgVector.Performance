9991fa63ef87b9804dadbffc426b2cf424c9a142
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-04-25 03:06:16 +0800

    Maximum Pool Size=100

--- VectorDataBases.Performance/GlobalManager.cs
+++ VectorDataBases.Performance/GlobalManager.cs
@@ -5,7 +5,7 @@ public static class GlobalManager
     // https://www.npgsql.org/doc/connection-string-parameters.html#pooling
     // https://stackoverflow.com/questions/44272459/postgres-npgsql-connection-pooling
     public static string postgreSQLConnectionString =
-        "Host=localhost;Database=pgvectors;Pooling=true;Minimum Pool Size=0;Maximum Pool Size=10;User Id=sa;Password=2022db@Qwer";
+        "Host=localhost;Database=pgvectors;Pooling=true;Minimum Pool Size=0;Maximum Pool Size=100;User Id=sa;Password=2022db@Qwer";
     public static string SelfHostRedisConnectionString = "localhost, password=p@$$w0rdwith0ut";
     public static string AzureRedisConnectionString = "awesomeyuer.eastus.redisenterprise.cache.azure.net:10000, password=ethDvIX6Ccc4qL8kq7Wsp7X2aNObRiXN28MnDxaKm0c=";
 
