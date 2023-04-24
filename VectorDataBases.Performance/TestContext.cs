c0cf39720c7b2aa5e8435b7d7ff70bd8b1b64cf3
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-04-25 02:10:33 +0800

    OKOK

--- VectorDataBases.Performance/TestContext.cs
+++ VectorDataBases.Performance/TestContext.cs
@@ -277,7 +277,7 @@ LIMIT $2;
             {
                 if (keyValuePair.Key == "vector_score")
                 {
-                    Console.WriteLine($"id: {document.Id}, score: {keyValuePair.Value}");
+                    //Console.WriteLine($"id: {document.Id}, score: {keyValuePair.Value}");
                 }
             }
         }
