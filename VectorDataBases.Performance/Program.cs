c9007296642f1b2b62df0d37dd439e860fd055d8
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-04-27 16:12:03 +0800

    1

--- VectorDataBases.Performance/Program.cs
+++ VectorDataBases.Performance/Program.cs
@@ -10,8 +10,9 @@ public class Program
         //GlobalManager.ConnectionString = string.Format(GlobalManager.ConnectionString, args[0]);
         //new TestContext().WikipediaPostgreSQL_ivfflat_vector_cosine_index_25k_ProcessAsync().Wait();
         ////new TestContext().WikipediaSelfHostRediSearch_FLAT_index_Cosine_25k_ProcessAsync().Wait();
-        new TestContext().RediSearch_HNSW_index_Cosine_225k_ProcessAsync().Wait();
-        //_ = BenchmarkRunner.Run<TestContext>();
+        //new TestContext().RediSearch_HNSW_index_Cosine_225k_ProcessAsync().Wait();
+        _ = BenchmarkRunner.Run<TestContext>();
+        Console.WriteLine($"{nameof(BenchmarkRunner)}.{nameof(BenchmarkRunner.Run)} done!");
         Console.ReadLine();
     }
     
